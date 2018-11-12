72501b5768739450dc181e82d5af6a1c4c1bd942
Zlatko Gudasić <zlatko.gudasic@gmail.com>
2018-11-12 14:44:21 +0100

    documentation for kaba-client module

--- kaba-client/src/main/java/hr/kaba/olb/client/kaba/App.java
+++ kaba-client/src/main/java/hr/kaba/olb/client/kaba/App.java
@@ -13,9 +13,18 @@ import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+/**
+ * Client that starts olb client and registers oracle responder as transaction responding service
+ * Start and stop are expected to be called outside from windows service or as linux process
+ *
+ * @author  Zlatko Gudasić
+ * @version 1.0
+ * @since   12.11.2018
+ */
 public enum App {
 
-    INSTANCE;
+    INSTANCE,
+    ;
 
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
@@ -27,6 +36,11 @@ public enum App {
     private OlbClient olbClient;
 
 
+    /**
+     * wrapper for start method
+     *
+     * @param args array of service starting arguments
+     */
     public static void start(String[] args) {
         try {
             INSTANCE.start();
@@ -38,6 +52,11 @@ public enum App {
         }
     }
 
+    /**
+     * wrapper for stop method
+     *
+     * @param args array of service stopping arguments
+     */
     public static void stop(String[] args) {
         try {
             INSTANCE.stop();
@@ -48,6 +67,15 @@ public enum App {
     }
 
 
+    /**
+     * # Loads properties for Oracle database connection
+     * # starts database
+     * # registers oracle responder with olb client
+     * # starts network client to listen for connection
+     *
+     * @throws IOException
+     * @throws InterruptedException
+     */
     public void start() throws IOException, InterruptedException {
 
         shouldRun = new AtomicBoolean(false);
@@ -72,6 +100,13 @@ public enum App {
 
     }
 
+    /**
+     * # Marks flag signifying that service should not run anymore
+     * # stops network client
+     * # closes database connection
+     *
+     * @throws InterruptedException
+     */
     public void stop() throws InterruptedException {
 
         shouldRun.set(false);
--- kaba-client/src/main/java/hr/kaba/olb/client/kaba/AppWrapper.java
+++ kaba-client/src/main/java/hr/kaba/olb/client/kaba/AppWrapper.java
@@ -3,6 +3,14 @@ package hr.kaba.olb.client.kaba;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Wrapper for starting local client for testing purposes
+ * Idea is to leave connection open for half a minute to test one or two transactions
+ *
+ * @author  Zlatko Gudasić
+ * @version 1.0
+ * @since   12.11.2018
+ */
 public class AppWrapper {
     public static void main(String[] args) throws IOException, InterruptedException {
         App.INSTANCE.start();
